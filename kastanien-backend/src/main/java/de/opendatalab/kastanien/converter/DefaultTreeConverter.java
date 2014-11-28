01d5f9691ca7a905be613865dd0d69173335ff46
Adrian Stabiszewski <as@grundid.de>
2014-11-28 14:51:25 +0100

    build fixes

--- kastanien-backend/src/main/java/de/opendatalab/kastanien/converter/DefaultTreeConverter.java
+++ kastanien-backend/src/main/java/de/opendatalab/kastanien/converter/DefaultTreeConverter.java
@@ -1,20 +1,22 @@
 package de.opendatalab.kastanien.converter;
 
-import de.opendatalab.kastanien.GeoPoint;
-import de.opendatalab.kastanien.Tree;
 import org.geojson.Feature;
 import org.geojson.LngLatAlt;
 import org.geojson.MultiPoint;
 
+import de.opendatalab.kastanien.GeoPoint;
+import de.opendatalab.kastanien.Tree;
+
 public class DefaultTreeConverter extends TreeConverter {
 
-	@Override public Tree toTree(Feature feature) {
-		Tree tree = new Tree();
-		tree.setTreeType(feature.getProperty("BAUMART"));
-		MultiPoint multiPoint = (MultiPoint)feature.getGeometry();
-		LngLatAlt lngLatAlt = multiPoint.getCoordinates().get(0);
-		tree.setLocation(new GeoPoint(lngLatAlt.getLatitude(), lngLatAlt.getLongitude()));
-		convertProperties(feature, tree);
-		return tree;
-	}
+    @Override
+    public Tree toTree(Feature feature) {
+        Tree tree = new Tree();
+        tree.setTreeType((String) feature.getProperty("BAUMART"));
+        MultiPoint multiPoint = (MultiPoint) feature.getGeometry();
+        LngLatAlt lngLatAlt = multiPoint.getCoordinates().get(0);
+        tree.setLocation(new GeoPoint(lngLatAlt.getLatitude(), lngLatAlt.getLongitude()));
+        convertProperties(feature, tree);
+        return tree;
+    }
 }
