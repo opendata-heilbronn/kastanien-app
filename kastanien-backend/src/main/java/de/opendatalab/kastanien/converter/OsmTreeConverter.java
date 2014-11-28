f82004a20a4c25d533b68d5553b974cadc785e2e
Adrian Stabiszewski <as@grundid.de>
2014-11-28 13:11:39 +0100

    java version set to 1.7 and type cast fix

--- kastanien-backend/src/main/java/de/opendatalab/kastanien/converter/OsmTreeConverter.java
+++ kastanien-backend/src/main/java/de/opendatalab/kastanien/converter/OsmTreeConverter.java
@@ -1,24 +1,25 @@
 package de.opendatalab.kastanien.converter;
 
-import de.opendatalab.kastanien.GeoPoint;
-import de.opendatalab.kastanien.Tree;
 import org.geojson.Feature;
 import org.geojson.LngLatAlt;
 import org.geojson.Point;
 
+import de.opendatalab.kastanien.GeoPoint;
+import de.opendatalab.kastanien.Tree;
+
 public class OsmTreeConverter extends TreeConverter {
 
-	@Override
-	public Tree toTree(Feature feature) {
-		Tree tree = new Tree();
-		tree.setTreeType(feature.getProperty("genus:de"));
-		if (tree.getTreeType() == null) {
-			tree.setTreeType(feature.getProperty("genus"));
-		}
-		Point multiPoint = (Point)feature.getGeometry();
-		LngLatAlt lngLatAlt = multiPoint.getCoordinates();
-		tree.setLocation(new GeoPoint(lngLatAlt.getLatitude(), lngLatAlt.getLongitude()));
-		convertProperties(feature, tree);
-		return tree;
-	}
+    @Override
+    public Tree toTree(Feature feature) {
+        Tree tree = new Tree();
+        tree.setTreeType((String) feature.getProperty("genus:de"));
+        if (tree.getTreeType() == null) {
+            tree.setTreeType((String) feature.getProperty("genus"));
+        }
+        Point multiPoint = (Point) feature.getGeometry();
+        LngLatAlt lngLatAlt = multiPoint.getCoordinates();
+        tree.setLocation(new GeoPoint(lngLatAlt.getLatitude(), lngLatAlt.getLongitude()));
+        convertProperties(feature, tree);
+        return tree;
+    }
 }
